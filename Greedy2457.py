1540037842089d9d3ddcc82e66c7c9a0607021e7
guswns96 <115156803+guswns96@users.noreply.github.com>
2022-12-05 00:13:30 +0900

    [2022-12-04]
    update Greedy2457

--- Greedy2457.py
+++ Greedy2457.py
@@ -7,9 +7,11 @@ for _ in range(n):
     day_list.append([start_m * 100 + start_d, end_m * 100 + end_d])
 day_list.sort()
 count_flo=0
-for i in range(len(day_list)):
+for i in range(n):
     max_num = 0
     temp=[]
+    if (end_day >= 1201 or day_list[0][0] > end_day):
+        break
     for j in range(len(day_list)):
         if day_list[j][0]<=end_day and day_list[j][1]>end_day:
             
@@ -21,7 +23,6 @@ for i in range(len(day_list)):
         elif day_list[j][0]>end_day:
             break
     end_day = max_num
-    print(end_day)
     del day_list[:len(temp)]
     count_flo+=1   
     if end_day>1201:
